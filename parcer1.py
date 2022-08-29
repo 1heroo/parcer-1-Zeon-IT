69baa2bbe71aaa8e7b32c105237bb63a1057e5ae
1heroo <106371215+1heroo@users.noreply.github.com>
2022-08-29 15:32:03 +0600

    Update parcer1.py
    redone

--- parcer1.py
+++ parcer1.py
@@ -2,7 +2,10 @@ from urllib import request
 from bs4 import BeautifulSoup
 import json
 
-url = 'http://newsline.kg/getNews.php?limit=30&last_dt=2022-07-04%2007:57:33.933739'
+limit = '30'
+last_dt = '2022-07-04%2007:57:33.933739'
+url = f'http://newsline.kg/getNews.php?limit={limit}&last_dt={last_dt}'
+
 
 html = request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
